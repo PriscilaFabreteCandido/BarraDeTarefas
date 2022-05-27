d89574d856b324cc98d1001d98192c0234ca4211
PriscilaFabreteCandido <priscilafabrete@gmail.com>
2022-05-27 11:37:45 -0300

    novo

---
+++ src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,47 @@
+
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Column } from 'src/app/pages/models/column.model';
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss']
+})
+export class MainViewComponent implements OnInit {
+  listaTarefa: Column[] = [];
+
+ constructor(
+
+
+  ) { }
+
+  ngOnInit(): void{
+    this.listaTarefa = [
+
+    ]
+  }
+  adicionar(nomeTarefa: string){
+    if(nomeTarefa.trim().length == 0){
+      return;
+    }
+    const tarefaEncontrada = this.listaTarefa.find(item => item.name.toLowerCase() == nomeTarefa.toLowerCase());
+    if (!tarefaEncontrada){
+      this.listaTarefa.push({id: this.listaTarefa.length, name: nomeTarefa});
+    }
+
+  }
+  excluir(id: number ){
+    this.listaTarefa = this.listaTarefa.filter(item => (item.id != id));
+  }
+  drop(event: CdkDragDrop<Column[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+}
